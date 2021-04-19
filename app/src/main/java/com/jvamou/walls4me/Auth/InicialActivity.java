305f0db247a756c91bda45877283e7dfe5148586
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-04-19 22:45:05 +0200

    ADD: creo la activity InicialActivity

---
+++ app/src/main/java/com/jvamou/walls4me/Auth/InicialActivity.java
@@ -0,0 +1,16 @@
+package com.jvamou.walls4me.Auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jvamou.walls4me.R;
+
+public class InicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_inicial);
+    }
+}
