6ae99c62741bec27281d2228025cb79707f63727
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-04-26 21:12:26 +0200

    ADD: añado splash

--- app/src/main/java/com/jvamou/walls4me/Inicial.java
+++ app/src/main/java/com/jvamou/walls4me/Inicial.java
@@ -3,6 +3,12 @@ package com.jvamou.walls4me;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Inicial extends AppCompatActivity {
 
