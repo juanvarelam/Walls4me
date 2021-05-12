647bc93a09f2ac41e036d3376b307b134fee50d2
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-05-12 21:16:20 +0200

    ADD: creo variables de registro para hacer la autenticación y las referencio

--- app/src/main/java/com/jvamou/walls4me/RegisterActivity.java
+++ app/src/main/java/com/jvamou/walls4me/RegisterActivity.java
@@ -3,12 +3,23 @@ package com.jvamou.walls4me;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    //variables para datosd e registro
+    private EditText authEmail;
+    private EditText authPassword;
+    private EditText authPasswordConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_register);
+
+        //referencia a los elementos
+        authEmail = findViewById(R.id.act_register_txt_email);
+        authPassword = findViewById(R.id.act_register_txt_password);
+        authPasswordConfirm = findViewById(R.id.act_register_txt_password_confirm);
     }
 }
