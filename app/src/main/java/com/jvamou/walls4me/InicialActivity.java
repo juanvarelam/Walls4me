a8e0835d1fdf8caddf7053ba030480784d30b894
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-05-15 20:42:40 +0200

    ADD: listeners en botones de InicialActivity para llevar al usuario a registrarse o iniciar sesión

--- app/src/main/java/com/jvamou/walls4me/InicialActivity.java
+++ app/src/main/java/com/jvamou/walls4me/InicialActivity.java
@@ -2,20 +2,47 @@ package com.jvamou.walls4me;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class InicialActivity extends AppCompatActivity {
 
+    private Button btnIniciarSesion;
+    private Button btnRegistrarse;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_inicial);
+
+        btnIniciarSesion = findViewById(R.id.act_inicial_btn_login);
+        btnRegistrarse = findViewById(R.id.act_inicial_btn_registrase);
+
+        //listener que al pulsar botón de iniciar sesión redirecciona a activity correspondiente
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicialActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //listener que al pulsar botón de registrarse redirecciona a activity correspondiente
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InicialActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
--- app/src/main/java/com/jvamou/walls4me/LoginActivity.java
+++ app/src/main/java/com/jvamou/walls4me/LoginActivity.java
@@ -1,14 +1,88 @@
 package com.jvamou.walls4me;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText authEmail;
+    private EditText authPassword;
+    private Button iniciarSesion;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_login);
+
+        authEmail = findViewById(R.id.act_login_email);
+        authPassword = findViewById(R.id.act_login_password);
+        iniciarSesion = findViewById(R.id.act_login_btn_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        iniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validar();
+            }
+        });
+    }
+
+    public void validar() {
+        String email = authEmail.getText().toString().trim().toLowerCase();
+        String password = authPassword.getText().toString().trim();
+
+        //la librería matcher comprueba que tenga estructura de email
+        if(email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            authEmail.setError("Correo  inválido");
+            return;
+        } else {  //continúa, no muestra  nada en el mensaje de error
+            authEmail.setError(null);
+        }
+
+        if(password.isEmpty() ||  password.length() < 8 ) {
+            authPassword.setError("La longitud mínima es de 8 caracteres");
+            return;
+        } else if (!Pattern.compile("[0-9]").matcher(password).find())  {
+            //comprueba que tenga un número
+            authPassword.setError("Debe contener un número");
+            return;
+        } else {
+            authPassword.setError(null);
+        }
+
+        iniciarSesion(email, password);
+    }
+
+    public void iniciarSesion(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Email o contraseña incorrectos", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
