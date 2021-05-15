316dae3400cf93399b72d172827fce963c60dc02
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-05-15 18:43:13 +0200

    ADD: método registrar que redirecciona a HomeActivity si la tarea fue correcta

--- app/src/main/java/com/jvamou/walls4me/RegisterActivity.java
+++ app/src/main/java/com/jvamou/walls4me/RegisterActivity.java
@@ -1,13 +1,19 @@
 package com.jvamou.walls4me;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.regex.Pattern;
@@ -75,6 +81,18 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void registrar(String email, String password) {
-
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Error al registrarse", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
