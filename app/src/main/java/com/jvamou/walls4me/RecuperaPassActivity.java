aef39421857d67a840f84f50e98f63740086a480
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-05-16 16:59:31 +0200

    ADD: método que envía correo de recuperación de contraseña y creo la plantilla de este en Firebase

--- app/src/main/java/com/jvamou/walls4me/RecuperaPassActivity.java
+++ app/src/main/java/com/jvamou/walls4me/RecuperaPassActivity.java
@@ -1,12 +1,19 @@
 package com.jvamou.walls4me;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RecuperaPassActivity extends AppCompatActivity {
 
@@ -37,6 +44,36 @@ public class RecuperaPassActivity extends AppCompatActivity {
             return;
         }
 
+        enviarCorreo(correo);
     }
 
+    //método que al hacer click en ir atrás nos redirecciona a LoginActivity
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Intent intent = new Intent(RecuperaPassActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void enviarCorreo(String correo) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String direccionCorreo = correo;
+
+        auth.sendPasswordResetEmail(direccionCorreo)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RecuperaPassActivity.this, "Correo enviado!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RecuperaPassActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(RecuperaPassActivity.this, "Correo inválido!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 }
