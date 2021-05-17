39006686dfab3d778961b424f1f4dafca04a58e8
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-05-17 16:44:05 +0200

    ADD: clase Wallpaper.java

---
+++ app/src/main/java/com/jvamou/walls4me/Wallpaper.java
@@ -0,0 +1,18 @@
+package com.jvamou.walls4me;
+
+public class Wallpaper {
+
+    int id;
+
+    public Wallpaper(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
