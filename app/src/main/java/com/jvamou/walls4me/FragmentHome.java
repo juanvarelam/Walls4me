da06783a056a1326ce1bfb2d7a0d99dc922658b5
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-05-17 21:29:48 +0200

    ADD: declaro variables en FragmentHome

--- app/src/main/java/com/jvamou/walls4me/FragmentHome.java
+++ app/src/main/java/com/jvamou/walls4me/FragmentHome.java
@@ -1,18 +1,41 @@
 package com.jvamou.walls4me;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.LinearLayout;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentHome extends Fragment {
 
-    RecyclerView recycler;
+    RecyclerView recyclerView;
+
+    private DatabaseReference dbRef;
+
+    private ArrayList<Wallpaper> wallpapersList;
+    private AdapterWallpaper adapterWallpaper;
+    private Context mContext;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
