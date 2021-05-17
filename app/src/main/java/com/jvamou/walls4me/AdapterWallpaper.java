5e225efb2d1571705d3df00054eed04e5f685ab9
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-05-17 17:00:28 +0200

    ADD: implemento métodos en AdapterWallpaper.java y creo layout para los ítems del RecyclerView

--- app/src/main/java/com/jvamou/walls4me/AdapterWallpaper.java
+++ app/src/main/java/com/jvamou/walls4me/AdapterWallpaper.java
@@ -1,16 +1,37 @@
 package com.jvamou.walls4me;
 
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AdapterWallpaper {
+public class AdapterWallpaper extends RecyclerView.Adapter<AdapterWallpaper.WallpaperViewHolder>{
+
+    @NonNull
+    @Override
+    public WallpaperViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WallpaperViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public static class WallpaperViewHolder extends RecyclerView.ViewHolder {
 
+        ImageButton imageButton;
+
         public WallpaperViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageButton = itemView.findViewById(R.id.item_wallpaper_individual);
         }
     }
 }
