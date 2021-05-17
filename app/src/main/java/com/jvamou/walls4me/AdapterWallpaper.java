57ef5e7655379403e1ec3f1a70bf300645bc4b93
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-05-17 16:47:27 +0200

    ADD: AdapterWallpaper.javay  método ViewHolder

---
+++ app/src/main/java/com/jvamou/walls4me/AdapterWallpaper.java
@@ -0,0 +1,16 @@
+package com.jvamou.walls4me;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterWallpaper {
+
+    public static class WallpaperViewHolder extends RecyclerView.ViewHolder {
+
+        public WallpaperViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
