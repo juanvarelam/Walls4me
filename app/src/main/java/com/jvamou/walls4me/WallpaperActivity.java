d04df6269bba077e786f2e3bd505d7dcf53b6028
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-06-03 20:15:34 +0200

    ADD: onClickListener en botón de cerrar de la activity act_wallpaper, que vuelve a mostrar el fragment del recyclerView

--- app/src/main/java/com/jvamou/walls4me/WallpaperActivity.java
+++ app/src/main/java/com/jvamou/walls4me/WallpaperActivity.java
@@ -2,13 +2,17 @@ package com.jvamou.walls4me;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class WallpaperActivity extends AppCompatActivity {
 
     ImageView imgFondo;
+    ImageButton btnCerrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,19 @@ public class WallpaperActivity extends AppCompatActivity {
         setContentView(R.layout.act_wallpaper);
 
         imgFondo = findViewById(R.id.act_wallpaper_fondo);
+        btnCerrar = findViewById(R.id.act_wallpaper_btn_cerrar);
+
+        btnCerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
 
         Bundle bundle = getIntent().getExtras();
         String url = bundle.getString("url");
 
-        imgFondo.setImageURI(Uri.parse(url));
+        imgFondo.setImageDrawable(Drawable.createFromPath(url));
 
     }
 }
