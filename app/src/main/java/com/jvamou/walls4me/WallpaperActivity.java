7365c7e600a76f0e50083f9288d69f62118f184e
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-06-03 19:46:26 +0200

    ADD: recojo la imagen seleccionada del RecyclerView y la muestro como fondo en el layout act_wallpaper.xml (aparece en blanco)

--- app/src/main/java/com/jvamou/walls4me/WallpaperActivity.java
+++ app/src/main/java/com/jvamou/walls4me/WallpaperActivity.java
@@ -2,13 +2,25 @@ package com.jvamou.walls4me;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class WallpaperActivity extends AppCompatActivity {
+
+    ImageView imgFondo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_wallpaper);
+
+        imgFondo = findViewById(R.id.act_wallpaper_fondo);
+
+        Bundle bundle = getIntent().getExtras();
+        String url = bundle.getString("url");
+
+        imgFondo.setImageURI(Uri.parse(url));
+
     }
 }
