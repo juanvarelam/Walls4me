879c81652e7383e4ee17535c8dc046b8b9d6e75e
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-06-03 20:19:20 +0200

    FIX: sustituyo onBackPressed() por finish() al cerrar la activity act_wallpaper

--- app/src/main/java/com/jvamou/walls4me/WallpaperActivity.java
+++ app/src/main/java/com/jvamou/walls4me/WallpaperActivity.java
@@ -25,7 +25,7 @@ public class WallpaperActivity extends AppCompatActivity {
         btnCerrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onBackPressed();
+                finish();
             }
         });
 
