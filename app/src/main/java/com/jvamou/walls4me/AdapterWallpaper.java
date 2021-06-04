9bbda964e68233981e01c9223de9370fd6bc6ccd
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-06-04 09:21:05 +0200

    DEL: elimino imports innecesarios

--- app/src/main/java/com/jvamou/walls4me/AdapterWallpaper.java
+++ app/src/main/java/com/jvamou/walls4me/AdapterWallpaper.java
@@ -1,16 +1,12 @@
 package com.jvamou.walls4me;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
