cc132d1bea22a0dfdcc57d49e7617fb8b14e7895
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-06-08 01:12:05 +0200

    ADD: FragmentCategorias

--- app/src/main/java/com/jvamou/walls4me/FragmentCategorias.java
+++ app/src/main/java/com/jvamou/walls4me/FragmentCategorias.java
@@ -2,63 +2,124 @@ package com.jvamou.walls4me;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentCategorias#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+
 public class FragmentCategorias extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    RecyclerView recyclerCategorias;
+    ArrayList<Categoria> listaCategorias;
+    AdapterCategorias adapterCategorias;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private DatabaseReference dbRef;
 
     public FragmentCategorias() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentCategorias.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentCategorias newInstance(String param1, String param2) {
-        FragmentCategorias fragment = new FragmentCategorias();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        //Instanciar Firebase
+        dbRef = FirebaseDatabase.getInstance().getReference("categorias");
+
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Categoria categoria = dataSnapshot.getValue(Categoria.class);
+                    listaCategorias.add(categoria);
+                }
+                adapterCategorias = new AdapterCategorias(listaCategorias, getContext());
+                recyclerCategorias.setAdapter(adapterCategorias);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.frg_categorias, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.frg_categorias, container, false);
+
+        listaCategorias = new ArrayList<>();
+        recyclerCategorias = v.findViewById(R.id.frg_categorias_recycler_categorias);
+
+        adapterCategorias = new AdapterCategorias(listaCategorias, getContext());
+        recyclerCategorias.setAdapter(adapterCategorias);
+        recyclerCategorias.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        recyclerCategorias.setHasFixedSize(true);
+
+        ObtenerDatosFirebase();
+
+        return v;
+    }
+
+    private void ObtenerDatosFirebase() {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("categorias").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                if(task.isSuccessful()) {
+                    limpiarDatos();
+                    for(DocumentSnapshot doc: task.getResult().getDocuments()) {
+                        Categoria categoria = new Categoria();
+                        categoria.url = doc.getString("url");
+                        listaCategorias.add(categoria);
+
+                        adapterCategorias = new AdapterCategorias(listaCategorias, getContext());
+                        recyclerCategorias.setAdapter(adapterCategorias);
+                        adapterCategorias.notifyDataSetChanged();
+                    }
+                }else{
+                    String error = task.getException().getLocalizedMessage();
+                    Log.e("FIREBASE", error);
+                }
+            }
+        });
+    }
+
+
+    private void limpiarDatos() {
+        if (listaCategorias != null) {
+            listaCategorias.clear();
+
+            if (adapterCategorias != null) {
+                adapterCategorias.notifyDataSetChanged();
+            }
+        }
+
+        listaCategorias = new ArrayList<>();
     }
 }
