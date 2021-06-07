3d3253e474700f31bc84e70377e9b9e8ef1ed582
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-06-07 20:39:40 +0200

    ADD: método para establecer el fondo de pantalla seleccionado al pulsar un botón

--- app/src/main/java/com/jvamou/walls4me/WallpaperActivity.java
+++ app/src/main/java/com/jvamou/walls4me/WallpaperActivity.java
@@ -3,27 +3,38 @@ package com.jvamou.walls4me;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Environment;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.File;
+import java.io.IOException;
 
 public class WallpaperActivity extends AppCompatActivity {
 
     ImageView imgFondo;
-    ImageButton btnCerrar;
     TextView texto;
-    ImageButton btnFavoritos;
-    Button btnEstablecerFondo;
-    Button btnVer;
-    Button btnDescargar;
-    Button btnMostrarOpciones;
+    ImageButton btnCerrar, btnFavoritos;
+    Button btnEstablecerFondo, btnVer, btnDescargar, btnMostrarOpciones;
+    Bitmap bitmap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +49,15 @@ public class WallpaperActivity extends AppCompatActivity {
         btnVer = findViewById(R.id.act_wallpaper_btn_ver_wallpaper);
         btnDescargar = findViewById(R.id.act_wallpaper_btn_descargar);
         btnMostrarOpciones = findViewById(R.id.act_wallpaper_btn_mostrar_opciones);
+
+        Bundle bundle = getIntent().getExtras();
+        String url = bundle.getString("url");
+
+        Glide.with(this)
+                .load(url)
+                .placeholder(R.drawable.ic_imagen_ph)
+                .error(R.drawable.ic_imagen_error)
+                .into(imgFondo);
 
         btnCerrar.setVisibility(View.VISIBLE);
         texto.setVisibility(View.VISIBLE);
@@ -88,14 +108,37 @@ public class WallpaperActivity extends AppCompatActivity {
             });
         });
 
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(WallpaperActivity.this);
 
-        Bundle bundle = getIntent().getExtras();
-        String url = bundle.getString("url");
+        btnEstablecerFondo.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceType")
+            @Override
+            public void onClick(View v) {
+                establecerFondo();
+            }
+        });
 
-        Glide.with(this)
-                .load(url)
-                .placeholder(R.drawable.ic_imagen_ph)
-                .error(R.drawable.ic_imagen_error)
-                .into(imgFondo);
+    }
+
+    public void establecerFondo() {
+
+        /*Bundle bundle = getIntent().getExtras();
+        String url = bundle.getString("url");*/
+
+        Bitmap bitmap = ((BitmapDrawable) imgFondo.getDrawable()).getBitmap();
+        //Bitmap bitmap = BitmapFactory.decodeResource(getResources(), Integer.parseInt(url));
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+
+
+        try {
+            wallpaperManager.setBitmap(bitmap);
+            Toast.makeText(this, "Fondo de pantalla actualizado", Toast.LENGTH_LONG).show();
+        }
+
+        catch (IOException e) {
+            Toast.makeText(this, "Ha ocurrido un problema", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
