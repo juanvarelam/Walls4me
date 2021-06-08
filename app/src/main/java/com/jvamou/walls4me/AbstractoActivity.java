54503e443136aacbd5f8d88ad600284da647f777
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-06-08 06:16:41 +0200

    ADD: AbstractoActivity

--- app/src/main/java/com/jvamou/walls4me/AbstractoActivity.java
+++ app/src/main/java/com/jvamou/walls4me/AbstractoActivity.java
@@ -1,14 +1,124 @@
 package com.jvamou.walls4me;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class AbstractoActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    ImageButton btnRetroceder;
+    AdapterActAbstracto adapterActAbstracto;
+
+    ArrayList<Wallpaper> wallpapersList;
+
+    private DatabaseReference dbRef;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_abstracto);
+
+        btnRetroceder = findViewById(R.id.act_abstracto_btn_retroceder);
+        recyclerView = findViewById(R.id.act_abstracto_recycler_abstracto);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        wallpapersList = new ArrayList<>();
+
+        //Instanciar Firebase
+        dbRef = FirebaseDatabase.getInstance().getReference("abstracto");
+
+        adapterActAbstracto = new AdapterActAbstracto(wallpapersList, getApplicationContext());
+        recyclerView.setAdapter(adapterActAbstracto);
+
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Wallpaper wallpaper = dataSnapshot.getValue(Wallpaper.class);
+                    wallpapersList.add(wallpaper);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        obtenerDatosFirebase();
+
+        btnRetroceder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+
+    private void obtenerDatosFirebase() {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("abstracto").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                if(task.isSuccessful()) {
+                    limpiarDatos();
+                    for(DocumentSnapshot doc: task.getResult().getDocuments()) {
+                        Wallpaper wallpaper = new Wallpaper();
+                        wallpaper.url = doc.getString("url");
+                        wallpapersList.add(wallpaper);
+
+                        adapterActAbstracto = new AdapterActAbstracto(wallpapersList, getApplicationContext());
+                        recyclerView.setAdapter(adapterActAbstracto);
+                        adapterActAbstracto.notifyDataSetChanged();
+                    }
+                }else{
+                    String error = task.getException().getLocalizedMessage();
+                }
+            }
+        });
+    }
+
+
+    private void limpiarDatos() {
+        if (wallpapersList != null) {
+            wallpapersList.clear();
+
+            if (adapterActAbstracto != null) {
+                adapterActAbstracto.notifyDataSetChanged();
+            }
+        }
+
+        wallpapersList = new ArrayList<>();
     }
 }
