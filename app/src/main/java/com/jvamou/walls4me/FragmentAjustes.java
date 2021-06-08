09e00c26f010744465ca0e171d401381e261e419
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-06-08 06:19:45 +0200

    ADD: FragmentAjustes

---
+++ app/src/main/java/com/jvamou/walls4me/FragmentAjustes.java
@@ -0,0 +1,62 @@
+package com.jvamou.walls4me;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class FragmentAjustes extends Fragment {
+
+
+    TextView email;
+    Button btnCerrarSesion;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user != null) {
+            email.setText(user.getEmail());
+        }
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.frg_ajustes, container, false);
+
+        email = v.findViewById(R.id.act_ajustes_txt_email);
+        btnCerrarSesion = v.findViewById(R.id.act_ajustes_btn_cerrar_sesion);
+
+        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getContext(), InicialActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        return v;
+    }
+}
