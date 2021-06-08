9387c16b073849f4027ca360b2001fc6066923a0
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-06-08 13:46:50 +0200

    ADD: adapters para las activities de cada categoría

---
+++ app/src/main/java/com/jvamou/walls4me/AdapterActVehiculos.java
@@ -0,0 +1,72 @@
+package com.jvamou.walls4me;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class AdapterActVehiculos extends RecyclerView.Adapter<AdapterActVehiculos.ViewHolder> {
+
+    ArrayList<Wallpaper> wallpaperList;
+    Context context;
+
+    public AdapterActVehiculos(ArrayList<Wallpaper> wallpaperList, Context context) {
+        this.wallpaperList = wallpaperList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_wallpaper, null, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String url = wallpaperList.get(position).getUrl();
+        Glide.with(context)
+                .load(url)
+                .placeholder(R.drawable.ic_imagen_ph)
+                .error(R.drawable.ic_imagen_error)
+                .into(holder.imageView);
+
+        holder.gridLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, WallpaperActivity.class);
+                intent.putExtra("url", url);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return wallpaperList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        GridLayout gridLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.item_wallpaper_imagen);
+            gridLayout = itemView.findViewById(R.id.item_wallpaper_layout);
+        }
+    }
+}
