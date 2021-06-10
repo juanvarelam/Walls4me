5180104e3f861e4f05e73a4e298bfd2c0405f101
Juan Varela Mourelle <juancontactovm@gmail.com>
2021-06-10 10:58:34 +0200

    FIX: rediseño FragmentInfo y añado botón de aviso legal

--- app/src/main/java/com/jvamou/walls4me/FragmentInfo.java
+++ app/src/main/java/com/jvamou/walls4me/FragmentInfo.java
@@ -2,11 +2,11 @@ package com.jvamou.walls4me;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -19,11 +19,12 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
-public class FragmentAjustes extends Fragment {
+public class FragmentInfo extends Fragment {
 
 
     TextView email;
-    Button btnCerrarSesion;
+    Button btnAvisoLegal, btnCerrarSesion;
+    private final static String avisoLegal = "https://firebasestorage.googleapis.com/v0/b/walls4me-56a6b.appspot.com/o/aviso_legal_walls4me.html?alt=media&token=3a1b4277-3de0-4383-ad74-771d2cead171";
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -35,16 +36,26 @@ public class FragmentAjustes extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.frg_ajustes, container, false);
+        View v = inflater.inflate(R.layout.frg_info, container, false);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        email = v.findViewById(R.id.frg_ajustes_email);
-        btnCerrarSesion = v.findViewById(R.id.frg_ajustes_btn_cerrar_sesion);
+        email = v.findViewById(R.id.frg_info_email);
+        btnCerrarSesion = v.findViewById(R.id.frg_info_btn_cerrar_sesion);
+        btnAvisoLegal = v.findViewById(R.id.frg_info_btn_aviso_legal);
 
         if(user != null) {
             email.setText(user.getEmail());
         }
+
+        btnAvisoLegal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri url = Uri.parse(avisoLegal);
+                Intent intent = new Intent(Intent.ACTION_VIEW, url);
+                startActivity(intent);
+            }
+        });
 
         btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
