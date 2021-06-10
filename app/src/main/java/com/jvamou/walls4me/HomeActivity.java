cc0403a4effcdd2a4a8c0e36680cfa0c342267f1
juanvarelam <juancontactovm@gmail.com>
2021-06-10 11:45:57 +0200

    FIX: navegación entre fragments

--- app/src/main/java/com/jvamou/walls4me/HomeActivity.java
+++ app/src/main/java/com/jvamou/walls4me/HomeActivity.java
@@ -1,20 +1,19 @@
 package com.jvamou.walls4me;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class HomeActivity extends AppCompatActivity {
+
+    //Variables globales fragments bottomNavigation
+    Fragment fragmentInicio, fragmentCategorias, fragmentFavoritos, fragmentAjustes;
 
     BottomNavigationView btnNavigaton;
 
@@ -23,36 +22,40 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_home);
 
-        cargarFragment(new FragmentInicio());
+        fragmentInicio = new FragmentInicio();
+        fragmentCategorias = new FragmentCategorias();
+        fragmentFavoritos = new FragmentFavoritos();
+        fragmentAjustes = new FragmentInfo();
 
         //Listener que recibe el fragment seleccionado por el usuario
         btnNavigaton = findViewById(R.id.act_home_botton_navigation);
-        btnNavigaton.setOnNavigationItemSelectedListener(this);
+        btnNavigaton.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        cargarFragment(fragmentInicio);
+
     }
 
     //Método que le pasa a cargarFragment() el fragment seleccionado
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        Fragment fragment = null;
-
-        switch(item.getItemId()) {
-            case R.id.fragment_inicio:
-                fragment = new FragmentInicio();
-                break;
-            case R.id.fragment_categorias:
-                fragment = new FragmentCategorias();
-                break;
-            case R.id.fragment_favoritos:
-                fragment = new FragmentFavoritos();
-                break;
-            case R.id.fragment_info:
-                fragment = new FragmentInfo();
-                break;
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch(item.getItemId()) {
+                case R.id.fragment_inicio:
+                    cargarFragment(fragmentInicio);
+                    return true;
+                case R.id.fragment_categorias:
+                    cargarFragment(fragmentCategorias);
+                    return true;
+                case R.id.fragment_favoritos:
+                    cargarFragment(fragmentFavoritos);
+                    return true;
+                case R.id.fragment_info:
+                    cargarFragment(fragmentAjustes);
+                    return true;
+            }
+            return true;
         }
-
-        return cargarFragment(fragment);
-    }
+    };
 
     @Override
     public void onBackPressed() {
@@ -66,15 +69,11 @@ public class HomeActivity extends AppCompatActivity implements BottomNavigationV
 
     }
 
-
     //Método que reemplaza el fragment existente por el nuevo
-    public boolean cargarFragment(Fragment fragment) {
+    public void cargarFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.act_home_layout_container, fragment);
         transaction.commit();
-
-        return true;
     }
-
 
 }
